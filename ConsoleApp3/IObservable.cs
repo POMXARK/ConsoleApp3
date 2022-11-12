48c53392d3c29d03effbf3b48cc0790f6558525a
POMXARK <1997pom@gmail.com>
2022-11-12 12:12:13 +1000

    Add project files.

---
+++ ConsoleApp3/IObservable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// observeble наблюдают за всеми изменениями и каким то образом на них реагируют
+    /// </summary>
+    /// <typeparam name="TypeDefinition"></typeparam>
+    public interface IObservable<TypeDefinition>
+    {
+        /// <summary>
+        /// парамметр принимает подпичика
+        /// подписчики определяются IObserver
+        /// </summary>
+        /// <param name="type"></param>
+        void Subscibe(TypeDefinition observer);
+        void UnSubscibe(TypeDefinition observer);
+
+        /// <summary>
+        /// Запускает процедуру уведомления всех подписчиков (observeble)
+        /// парамер принимает информацию, которую нужно донести подписчику
+        /// например: новость
+        /// </summary>
+        /// <param name="type"></param>
+        void Notify(TypeDefinition data);
+    }
+}
---
+++ ConsoleApp3/IObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    /// <summary>
+    /// observe ( наблюдаталь - патерн проектирования который реализует механизм подписок)
+    /// </summary>
+    /// <typeparam name="TypeDefinition"></typeparam>
+    public interface IObserver<TypeDefinition>
+    {
+        /// <summary>
+        /// Этот метод будет вызываться, 
+        /// когда наблюдатель (observable) получает уведомление
+        /// Пример: агрегатор новостей ( агрегатор, подписчики - наблюдатели (читатели))
+        /// читатели получают уведомление от агрегатора при появлении новой новости
+        /// факт того что появилась новость хорошо бы сопроваждать ссылкой или самой новостью , параметр data отвечает за это 
+        /// </summary>
+        /// <param name="data"></param>
+       void Update(TypeDefinition data);
+    }
+}
